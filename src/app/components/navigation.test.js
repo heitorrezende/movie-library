60bc86b4b5087ab7e3ac71cb1ff3fa07d776b383
Heitor Rezende <heitorrezendeborba@gmail.com>
2024-10-14 16:55:24 -0300

    fix: ajust test

--- src/app/components/navigation.test.js
+++ src/app/components/navigation.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Navigation from './navigation';
 
@@ -13,34 +13,44 @@ jest.mock('next/link', () => ({
 }));
 
 describe('Navigation', () => {
-  test('renders the navigation bar', () => {
-    render(<Navigation />);
+  test('renders the navigation bar', async () => {
+    await act(async () => {
+      render(<Navigation />);
+    });
     expect(screen.getByRole('navigation')).toBeInTheDocument();
   });
 
-  test('renders the Home link', () => {
-    render(<Navigation />);
+  test('renders the Home link', async () => {
+    await act(async () => {
+      render(<Navigation />);
+    });
     const homeLink = screen.getByText('Home');
     expect(homeLink).toBeInTheDocument();
     expect(homeLink).toHaveAttribute('href', '/');
   });
 
-  test('renders the Movies link', () => {
-    render(<Navigation />);
+  test('renders the Movies link', async () => {
+    await act(async () => {
+      render(<Navigation />);
+    });
     const moviesLink = screen.getByText('Movies');
     expect(moviesLink).toBeInTheDocument();
     expect(moviesLink).toHaveAttribute('href', '/movies');
   });
 
-  test('renders the TV Shows link', () => {
-    render(<Navigation />);
+  test('renders the TV Shows link', async () => {
+    await act(async () => {
+      render(<Navigation />);
+    });
     const tvShowsLink = screen.getByText('TV Shows');
     expect(tvShowsLink).toBeInTheDocument();
     expect(tvShowsLink).toHaveAttribute('href', '/tvshows');
   });
 
-  test('applies the correct class to links', () => {
-    render(<Navigation />);
+  test('applies the correct class to links', async () => {
+    await act(async () => {
+      render(<Navigation />);
+    });
     const links = screen.getAllByRole('link');
     links.forEach(link => {
       expect(link).toHaveClass('hover:underline');
